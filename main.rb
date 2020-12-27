e3b3110c4bc123331db16b33b48b28b05af4e842
UraShu <b1019128@fun.ac.jp>
2020-12-27 23:20:36 +0900

    Add background

--- main.rb
+++ main.rb
@@ -1,9 +1,10 @@
 require 'dxopal'
 include DXOpal
+
 Window.load_resources do
-  Window.bgcolor = C_BLACK
 
   Window.loop do
-    Window.draw_font(0, 0, "Hello!", Font.default, color: C_WHITE)
+    Window.draw_box_fill(0, 0, Window.width, 50, [128, 128, 128] )
+    Window.draw_box_fill(0, 50, Window.width, Window.height, [C_BLACK])
   end
 end
