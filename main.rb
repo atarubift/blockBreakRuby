d4b0b2d640e0463315fb42ace1e753aa6e1be91c
UraShu <atarubift@gmail.com>
2020-12-28 19:24:43 +0900

    Add block

--- main.rb
+++ main.rb
@@ -18,6 +18,14 @@ walls = [Sprite.new(0, 50, Image.new(1, Y_HEIGHT - 50, C_BLACK)),
 ball = Sprite.new(X_WIDTH / 2, Y_HEIGHT - 40, Image.new(10, 10).circle_fill(5, 5, 5, C_WHITE))
 dx = 3
 dy = -3
+image = Image.new(58, 18, C_WHITE)
+blocks = []
+7.times do |y|
+  10.times do |x|
+    blocks << Sprite.new(21 + 60 * x, 55 + 20 * y, image) 
+  end
+end
+
 
 Window.load_resources do
 
@@ -47,8 +55,24 @@ Window.load_resources do
         dx = -dx
       end
 
+      x_block = ball.check(blocks).first
+      if x_block
+        x_block.vanish
+        GAME_INFO[:score] += 100
+        ball.x -= dx
+        dx = -dx
+      end
+
       ball.y += dy
       if ball === walls
+        ball.y -= dy
+        dy = -dy
+      end
+
+      y_block = ball.check(blocks).first
+      if y_block
+        y_block.vanish
+        GAME_INFO[:score] += 100
         ball.y -= dy
         dy = -dy
       end
@@ -65,9 +89,25 @@ Window.load_resources do
       end
 
       ball.draw
+      Sprite.draw(blocks)
     when :gameover 
       Window.draw_font((X_WIDTH / 5) * 2, (Y_HEIGHT - 50) / 3, "GAME OVER", Font.default)
-      Window.draw_font((X_WIDTH / 3) - 30, (Y_HEIGHT - 50) / 2, "RETRY:SPACE EXIT: ESC", Font.default)
+      Window.draw_font((X_WIDTH / 5) * 2, (Y_HEIGHT - 50) / 2, "RETRY:SPACE", Font.default)
+      if Input.key_push?(K_SPACE)
+        GAME_INFO[:score] = 0
+        GAME_INFO[:life] = 5
+        GAME_INFO[:scene] = :title
+        ball.x = X_WIDTH / 2
+        ball.y = Y_HEIGHT - 40
+        dx = 3
+        dy = -3
+        block = []
+        7.times do |y|
+          10.times do |x|
+            blocks << Sprite.new(21 + 60 * x, 55 + 20 * y, image) 
+          end
+        end
+      end
     end
   end
 end
